88081d9f95faf6f9806a978dec6c659c69f95c6c
Chiryen <1522055939@qq.com>
2017-05-14 19:47:46 +0800

    init

---
+++ src/main/java/com/nowcoder/controller/HomeController.java
@@ -0,0 +1,58 @@
+package com.nowcoder.controller;
+
+import com.nowcoder.model.Question;
+import com.nowcoder.model.ViewObject;
+import com.nowcoder.service.QuestionService;
+import com.nowcoder.service.UserService;
+import org.hibernate.validator.constraints.ParameterScriptAssert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by John on 2017/5/13.
+ */
+@Controller
+public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    QuestionService questionService;
+
+    @RequestMapping(path = {"/", "/index"})
+    public String index(Model model) {
+        List<Question> questionList = questionService.getLatestQuestions(0,0,10);
+        List<ViewObject> vos = getQuestions(0,0,10);
+        model.addAttribute("vos", vos);
+        return "index";
+    }
+
+    @RequestMapping(path = {"/user/{userId}"})
+    public String userIndex(Model model, @PathVariable("userId") int userId) {
+        model.addAttribute("vos", getQuestions(userId, 0, 10));
+        return "index";
+    }
+
+    private List<ViewObject> getQuestions(int userId, int offset, int limit) {
+        List<Question> questionList = questionService.getLatestQuestions(userId,offset,limit);
+        List<ViewObject> vos = new ArrayList<ViewObject>();
+        for(Question question : questionList) {
+            ViewObject vo = new ViewObject();
+            vo.set("question", question);
+            vo.set("user", userService.getUser(question.getUserId()));
+            vos.add(vo);
+        }
+        return vos;
+    }
+}
---
+++ src/main/java/com/nowcoder/service/UserService.java
@@ -0,0 +1,20 @@
+package com.nowcoder.service;
+
+import com.nowcoder.dao.UserDAO;
+import com.nowcoder.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by John on 2017/5/13.
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    UserDAO userDAO;
+
+    public User getUser(int id) {
+        return userDAO.selectById(id);
+    }
+}
