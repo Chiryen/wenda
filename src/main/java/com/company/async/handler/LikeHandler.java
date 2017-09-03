9ad6f709198de00fa5b4f4f510af65eaa93293d9
Chiryen <1522055939@qq.com>
2017-09-03 13:40:30 +0800

    别人对自己的赞的内容显示的修改

--- src/main/java/com/company/async/handler/LikeHandler.java
+++ src/main/java/com/company/async/handler/LikeHandler.java
@@ -5,6 +5,7 @@ import com.company.async.EventModel;
 import com.company.async.EventType;
 import com.company.model.Message;
 import com.company.model.User;
+import com.company.service.CommentService;
 import com.company.service.MessageService;
 import com.company.service.UserService;
 import com.company.util.WendaUtil;
@@ -27,6 +28,9 @@ public class LikeHandler implements EventHandler {
     @Autowired
     UserService userService;
 
+    @Autowired
+    CommentService commentService;
+
     @Override
     public void doHandle(EventModel model) {
         Message message = new Message();
@@ -34,7 +38,9 @@ public class LikeHandler implements EventHandler {
         message.setToId(model.getEntityOwnerId());
         message.setCreatedDate(new Date());
         User user = userService.getUser(model.getActorId());
-        message.setContent("用户" + user.getName() + "赞了你的评论， http://127.0.0.1:8080/question/" + model.getExt("questionId"));
+        //message.setContent("用户" + user.getName() + "赞了你的评论， http://127.0.0.1:8080/question/" + model.getExt("questionId"));
+        String commentContent = commentService.getCommentById(model.getEntityId()).getContent();
+        message.setContent("用户" + user.getName() + "赞了你的评论\"" + commentContent + "\"");
         messageService.addMessage(message);
     }
 
